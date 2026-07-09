55a4efa6dca24e5fce2d3271495e8bb04f217ca0
3illBaby <thrillanon@outlook.com>
2026-07-09 16:50:21 +0100

    chore(app.jsx): init

---
+++ src/sections/hero.jsx
@@ -0,0 +1,17 @@
+import GridBackground from "../shared/components/grid-background"
+
+const Hero = () => {
+  return (
+    <section className="flex flex-col w-full min-h-screen">
+      <GridBackground>
+        <h1 className="text-neutral-300 text-3xl font-bold font-bebas">
+          Work In Progress
+        </h1>
+      </GridBackground>
+    </section>
+  )
+}
+
+
+
+export default Hero
---
+++ src/shared/components/grid-background.jsx
@@ -0,0 +1,43 @@
+import { cn } from "@/lib/utils";
+
+
+
+const GridBackground = ({
+  children,
+  className,
+  variant = "default",
+}) => {
+  const getGridStyles = () => {
+    switch (variant) {
+      case "dark":
+        return "[background-image:linear-gradient(to_right,#404040_1px,transparent_1px),linear-gradient(to_bottom,#404040_1px,transparent_1px)]";
+      case "light":
+        return "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]";
+      default:
+        return cn(
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#404040_1px,transparent_1px),linear-gradient(to_bottom,#404040_1px,transparent_1px)]",
+        );
+    }
+  };
+
+  return (
+    <div
+      className={`relative flex h-fit w-full flex-col ${variant === "default" ? "bg-white dark:bg-black" : ""} ${className}`}
+    >
+      {/* Grid pattern background */}
+      <div
+        className={cn(
+          "absolute inset-0 z-0",
+          "bg-size[_20px_20px]",
+          getGridStyles(),
+        )}
+      />
+
+      {/* Content container */}
+      <div className="relative z-20">{children}</div>
+    </div>
+  );
+};
+
+export default GridBackground;
